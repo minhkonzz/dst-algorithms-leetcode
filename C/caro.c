197a353ed6e8e5ab14e5241f33b37b812cd116a1
minhkonzz <58776566+minhkonzz@users.noreply.github.com>
2021-12-01 16:43:38 +0700

    Update caro.c

--- C/caro.c
+++ C/caro.c
@@ -26,13 +26,31 @@ void print_state(state s) {
 	}
 }
 
-state * move(state s, int i, int j) {
+state * copy_state(state s) {
+	state * cs = (state*)malloc(sizeof(state));
+	if(cs != NULL) {
+		cs->n = s.n;
+		int i = 0, len = pow(s.n, 2);
+		cs->decis = (char*)malloc(len); 
+		while (i < len) {
+			*(cs->decis + i) = *(s.decis + i);
+			i++;
+		}
+	}
+	return cs; 
+}
+
+state * set(state s, int i, int j) {
 	if(i < 0 || i >= s.n || j < 0 || j >= s.n || *(s.decis + i * s.n + j) != '_') 
 		return NULL;
 	int c = 0, res = 0, len = pow(s.n, 2);
-	while(c < len)
+	while(c < len) {
 		if(*(s.decis + c) != '_') res += 1;
-	 
+		c++;
+	}
+	state * cs = copy_state(s);
+	*(cs->decis + i * cs->n + j) = (c % 2 == 0 ? 'O' : 'X');
+	return cs; 
 }
 
 int main(void) {
@@ -40,6 +58,11 @@ int main(void) {
 	init_state(&s, 3);
 	*(s.decis+0*3+1) = 'X';
 	*(s.decis+1*3+2) = 'O';
+	print_state(s);
+	printf("\n=====================\n");
+	state * new_s = set(s, 1, 1); 
+	print_state(*new_s);
+	printf("\n=====================\n");
 	print_state(s);
 	return 0;
 }
