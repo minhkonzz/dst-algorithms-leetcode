aad60ab9f9dcd2028719b9570dd6d0f1be2897c6
minhkonzz <minhphm37@gmail.com>
2025-03-12 23:04:43 +0700

    problems: longest-consecutive-sequence (medium)

---
+++ problems/medium/longest-consecutive-sequence/sol.js
@@ -0,0 +1,20 @@
+/**
+ * Time Complexity: O(n)
+ * Spa
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+const longestConsecutive = function (nums) {
+  if (nums.length == 0) return 0;
+  const numsSet = new Set(nums);
+  let maxLength = 0;
+  for (let e of nums) {
+    if (!numsSet.has(e - 1)) {
+      let length = 0;
+      while (numsSet.has(e++)) ++length;
+      maxLength = Math.max(maxLength, length);
+    }
+  }
+  return maxLength;
+};
